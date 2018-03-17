08790cbd39dbe76ddcd126e9f1f1142b2304508c
Josh Vargas <navyjosh88@gmail.com>
2018-03-17 16:01:50 -0400

    created login page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'login/index'
+
   resources :reviews
   resources :products
-  root 'products#index'
+  root 'login#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
