3cf09852403ccfb069d651983e8831ce1ea17ad5
Josh Vargas <navyjosh88@gmail.com>
2018-03-19 14:38:42 -0400

    fixed root

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   devise_for :users, path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
 
-  root 'products#new'
+  root 'products#index'
 
   resources :reviews
   resources :products
