7ce3cbd9abc3cb96e6e0defa87e5cb526cc8b9a6
Macus Desir <macusdesir@gmail.com>
2018-03-18 20:24:56 -0400

    corrected Josh's errors

--- config/routes.rb
+++ config/routes.rb
@@ -10,15 +10,8 @@ Rails.application.routes.draw do
   resources :products
 
   resources :cart, only: [:show] do
-<<<<<<< HEAD
     put 'add/:product_id', to: 'carts#add', as: :add_to
     put 'remove/:product_id', to: 'carts#remove', as: :remove_from
-=======
-    put 'add/:product_id', to: 'carts#add', as:
-  :add_to
-    put 'remove/:product_id' to: 'carts#remove', as:
-  :remove_from
->>>>>>> 18bb77200de8b3daa30859c677578c72aa0c9943
   end
 
   get '/products', to: 'products#index'
